1c0416207c0857fdc95a8e85b050dae75a1e67ec
rjmithcell92 <robertjames.mitchell@phs.scot>
2024-05-21 13:51:14 +0100

    Fix Postcode Issue

--- 3_Server_Scripts/2.4.9 - Postcode_Processing.R
+++ 3_Server_Scripts/2.4.9 - Postcode_Processing.R
@@ -19,7 +19,7 @@
       # If postcode is given in partial format, keep value
       App_data$values <- App_data$values %>% 
       mutate(POSTCODE = toupper(POSTCODE)) %>%
-      mutate(POSTCODE1 = postcode(POSTCODE, "pc7")) %>%
+      mutate(POSTCODE1 = format_postcode(POSTCODE, "pc7")) %>%
       mutate(POSTCODE2 = ifelse(is.na(POSTCODE1), POSTCODE, POSTCODE1)) %>%
       select(-POSTCODE,-POSTCODE1) %>%
       rename(POSTCODE = POSTCODE2)
